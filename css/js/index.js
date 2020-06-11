e3b667879155e7cf45524ded4fd0972c17eebcd7
diegoBenitez <diegof.17@hotmail.com>
2020-06-10 23:36:38 -0500

    version mobile 4/3

---
+++ css/js/index.js
@@ -0,0 +1,110 @@
+//JQuery
+$('.questions__item').click(function(){
+  $(this).children('.answer').slideToggle(); 
+})
+const $featuresCopy = document.getElementsByClassName('features__container__copys__copy');
+const $featuresItems = document.getElementsByClassName('features_items__a'); 
+const $featuresImgs = document.getElementsByClassName('features-img');
+const $selectedItem1 = document.getElementById('sltd1');
+const $selectedItem2 = document.getElementById('sltd2');
+const $selectedItem3 = document.getElementById('sltd3');
+const $quetions_item = document.getElementsByClassName('questions__item')
+const $svgArrow = document.getElementsByClassName('svgArrow')
+const $pathArrow = document.getElementsByClassName('pathsvg')
+const $hamburguer = document.getElementById('hamburguer')
+const $close = document.getElementById('close')
+const $nav = document.getElementById('navi')
+const $herder_logo =document.getElementById('header__logo')
+
+$hamburguer.addEventListener('click',()=>{
+ $nav.style.display ='block'
+ $hamburguer.style.display = 'none'
+ $herder_logo.style.display ='none'
+})
+
+$close.addEventListener('click',()=>{
+  $nav.style.display ='none'
+ $hamburguer.style.display = 'block'
+ $herder_logo.style.display ='block'
+})
+
+$featuresItems[1].addEventListener('click',function(){
+  $featuresCopy[0].style.animation ='copyout .5s ease forwards'
+  $featuresCopy[0].style.display ='none'
+  $featuresCopy[2].style.animation ='copyout .5s ease forwards'
+  $featuresCopy[2].style.display ='none'
+  $featuresCopy[1].style.animation ='copyin .5s ease forwards'; 
+  $featuresCopy[1].style.display ='block'
+  //forms selected
+  $selectedItem1.style.display='none'
+  $selectedItem2.style.display='block'
+  $selectedItem3.style.display='none'
+  //images
+  $featuresImgs[0].style.display='none';
+  $featuresImgs[0].style.right='100%';
+  $featuresImgs[2].style.display='none';
+  $featuresImgs[2].style.right='100%';
+  $featuresImgs[1].style.display='block';
+  $featuresImgs[1].style.right='1.6em';
+})
+$featuresItems[2].addEventListener('click',function(){
+  $featuresCopy[0].style.animation ='copyout .5s ease forwards'
+  $featuresCopy[0].style.display ='none'
+  $featuresCopy[1].style.animation ='copyout .5s ease forwards'
+  $featuresCopy[1].style.display ='none'
+  $featuresCopy[2].style.animation ='copyin .5s ease forwards'; 
+  $featuresCopy[2].style.display ='block'
+   //forms selected
+  $selectedItem1.style.display='none'
+  $selectedItem2.style.display='none'
+  $selectedItem3.style.display='block'
+  //images
+  $featuresImgs[0].style.display='none';
+  $featuresImgs[0].style.right='100%';
+  $featuresImgs[1].style.display='none';
+  $featuresImgs[1].style.right='100%';
+  $featuresImgs[2].style.display='block';
+  $featuresImgs[2].style.right='1.6em';
+})
+$featuresItems[0].addEventListener('click',function(){
+  $featuresCopy[1].style.animation ='copyout .5s ease forwards'
+  $featuresCopy[1].style.display ='none'
+  $featuresCopy[2].style.animation ='copyout .5s ease forwards'
+  $featuresCopy[2].style.display ='none'
+  $featuresCopy[0].style.animation ='copyin .5s ease forwards'; 
+  $featuresCopy[0].style.display ='block'
+ //forms selected
+  $selectedItem1.style.display='block'
+  $selectedItem2.style.display='none'
+  $selectedItem3.style.display='none'
+  //images
+  $featuresImgs[1].style.display='none';
+  $featuresImgs[1].style.right='100%';
+  $featuresImgs[2].style.display='none';
+  $featuresImgs[2].style.right='100%';
+  $featuresImgs[0].style.display='block';
+  $featuresImgs[0].style.right='1.6em';
+
+})
+//SECTION QUESTIONS
+$quetions_item[0].addEventListener('click',()=>{
+  $svgArrow[0].classList.toggle('svg')
+  $pathArrow[0].classList.toggle('path')
+})
+$quetions_item[1].addEventListener('click',()=>{
+  $svgArrow[1].classList.toggle('svg')
+  $pathArrow[1].classList.toggle('path')
+})
+$quetions_item[2].addEventListener('click',()=>{
+  $svgArrow[2].classList.toggle('svg')
+  $pathArrow[2].classList.toggle('path')
+})
+$quetions_item[3].addEventListener('click',()=>{
+  $svgArrow[3].classList.toggle('svg')
+  $pathArrow[3].classList.toggle('path')
+})
+  
+
+
+
+
